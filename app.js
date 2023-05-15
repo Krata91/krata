63334de1d0ff2b0f03fc96bcf66d172324431a72
Krata91 <kratochvilejan@gmail.com>
2023-05-15 11:10:44 +0200

    ok

---
+++ app.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "John",
+    age: 30,
+    hobbies: ["reading", "hiking", "cooking"],
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA"
+    }
+}
+const fullName = `${person.name} ${pesron.age}`;
+console.log(fullName);
+console.log("Jméno:",person.name);
+console.log(person.city);
